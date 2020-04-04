08e4877ee744cc977120bc07d043271cac5e5450
Kinachan <army1102@gmail.com>
2020-04-04 20:32:32 +0900

    検索バグ修正

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
+const defaultLaungage = {
+  sProcessing: "処理中...",
+  sLengthMenu: "_MENU_ 件表示",
+  sZeroRecords: "データはありません。",
+  sInfo: " _TOTAL_ 件中 _START_ から _END_ まで表示",
+  sInfoEmpty: " 0 件中 0 から 0 まで表示",
+  sInfoFiltered: "（全 _MAX_ 件より抽出）",
+  sInfoPostFix: "",
+  sSearch: "検索:",
+  sUrl: "",
+  oPaginate: {
+    sFirst: "先頭",
+    sPrevious: "前",
+    sNext: "次",
+    sLast: "最終"
+  },
+  decimal:",",
+  thousands: ".",
+};
+
 let isoList = null;
 
 function periodRound(value, base) {
   return Math.round(value * base) / base;
+}
+
+function withComma() {
+  return $.fn.dataTable.render.number(',',',');
 }
 
 $(document).ready(function () {
@@ -13,18 +37,24 @@ $(document).ready(function () {
 
   // datatableの設定を変更
   $('#corona').DataTable({
-    language: {
-      url: "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
-    },
+    render: $.fn.dataTable.render.number( ',', '.', 2, '' ),
+    language: defaultLaungage,
     ajax: 'https://corona-stats.online/?format=json',
     columns: [
-      { 'data': 'countryInfo', title: '', width: 20},
-      { 'data': 'country', title: '国', width: 30 },
-      { 'data': 'cases', title: '感染者', width: 40, 'className': 'number' },
-      { 'data': 'deaths', title: '死亡', width: 30, 'className': 'number'  },
-      { 'data': 'recovered', title: '回復者', width: 32, 'className': 'number'  },
-      { 'data': 'active', title: '感染中', width: 32, 'className': 'number'  },
-      { 'data': 'critical', title: '重症', width: 15, 'className': 'number'  },
+      // 英語検索用のデータ
+      { 'data': 'country', title: '', width: 20},
+      { 'data': 'country', title: '国', width: 30, render:(data,type,row) => {
+        const iso2 = row["countryInfo"].iso2;
+        const iso = isoList.find(x =>x.alpha2 === iso2);
+
+        if (iso == null) return data;
+        return iso.companyjp || data;
+      } },
+      { 'data': 'cases', title: '感染者', width: 40, 'className': 'number',render: withComma()},
+      { 'data': 'deaths', title: '死亡', width: 30, 'className': 'number', render: withComma()  },
+      { 'data': 'recovered', title: '回復者', width: 32, 'className': 'number', render: withComma()  },
+      { 'data': 'active', title: '感染中', width: 32, 'className': 'number', render:withComma()  },
+      { 'data': 'critical', title: '重症', width: 15, 'className': 'number',render: withComma()  },
       { 'data': 'deathRate', title: '死亡率', 'className': 'rate', width:40, 
         render:(data, type,row)=> {
           return periodRound(row['deaths'] / row['cases'] * 100, 100); 
@@ -35,7 +65,7 @@ $(document).ready(function () {
           if (row['cases'] === row['active']) return '-'; // 感染中と総件数が一緒の場合は計測不可
           return periodRound(row['recovered'] / row['cases'] * 100, 100);
         } 
-      }
+      },
     ],
     order: [[2, 'desc']],
     lengthMenu:[[50,10, 100,-1], [50,10, 100, '全']],
@@ -46,14 +76,6 @@ $(document).ready(function () {
       const iso2 = data.countryInfo.iso2 != null ? data.countryInfo.iso2.toLowerCase() : '';
       const icon = `<i class="flag-icon flag-icon-${iso2}"></i>`;
       flag.innerHTML = icon;
-
-      if (isoList == null) return;
-      const iso = isoList.find(x =>x.alpha2 === data.countryInfo.iso2);
-
-      if (iso != null) {
-        const country = $('td', row).eq(1)[0];
-        country.innerText = iso.companyjp
-      }
     }
   });
 });
