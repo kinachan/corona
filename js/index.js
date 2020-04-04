927192ae4fb3816a73dcefb3357ee500744ce601
Kinachan <army1102@gmail.com>
2020-04-04 16:12:20 +0900

    国旗を追加

--- js/index.js
+++ js/index.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
   // datatableの設定を変更
   $('#corona').DataTable({
     language: {
-      url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
+      url: "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json"
     },
-    'ajax': 'https://corona-stats.online/?format=json',
-    'columns': [
-      { 'data': 'country', title: '国', width: 40 },
+    ajax: 'https://corona-stats.online/?format=json',
+    columns: [
+      { 'data': 'countryInfo', title: '', width: 20},
+      { 'data': 'country', title: '国', width: 30 },
       { 'data': 'cases', title: '感染者', width: 40, 'className': 'number' },
       { 'data': 'todayCases', title: '感染者(本日)', width: 60, 'className': 'number'  },
       { 'data': 'deaths', title: '死亡', width: 30, 'className': 'number'  },
@@ -15,7 +16,14 @@ $(document).ready(function () {
       { 'data': 'active', title: '感染中', width: 32, 'className': 'number'  },
       { 'data': 'critical', title: '重症', width: 15, 'className': 'number'  },
     ],
-    'order': [[1, 'desc']],
-    'lengthMenu':[[50,10, 100,-1], [50,10, 100, 'ALL']],
+    order: [[2, 'desc']],
+    lengthMenu:[[50,10, 100,-1], [50,10, 100, '全']],
+    rowCallback: function (row, data) {
+      const target = $('td', row).eq(0)[0];
+
+      const country = data.countryInfo.iso2 != null ? data.countryInfo.iso2.toLowerCase() : '';
+      const icon = `<i class="flag-icon flag-icon-${country}"></i>`;
+      target.innerHTML = icon;
+    }
   });
 });
